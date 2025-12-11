65700ea83d0ae605abd07f62864391302b9e8554
Fabio  Jarjury <jarjuryfabio@gmail.com>
2025-12-11 00:23:51 -0400

    Update test.java

--- CalculoCoefB.java/Main.java
+++ CalculoCoefB.java/Main.java
@@ -1,13 +1,20 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        double[] x = {1, 2, 3, 4};
-        double[] y = {2, 3, 5, 7};
 
-        CalculoCoefB calc = new CalculoCoefB(x, y);
-        double B = calc.calcularB();
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
-        System.out.println("Coeficiente B: " + B);
+public class CoeficienteBTest {
+
+    @Test
+    public void testCalcularB() {
+
+        var xs = Arrays.asList(0.0, 0.2, 0.4, 0.6, 0.8, 1.0);
+        var ys = Arrays.asList(0.0, -0.485, -0.996, -1.271, -1.494, -1.571);
+
+        double B = CoeficienteB.calcularB(xs, ys);
+
+        assertEquals(-1.5740, B, 0.0001); 
     }
 }
