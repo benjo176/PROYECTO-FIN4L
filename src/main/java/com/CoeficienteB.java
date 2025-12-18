d0451ed5dddd1c293be17d01182e2c1c3089c0bd
benjo176 <angelofernandez455@gmail.com>
2025-12-18 14:19:45 +0100

    cambios para que corra CoeficienteBTest.java

---
+++ src/main/java/com/CoeficienteB.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.util.List;
+
+public class CoeficienteB {
+
+    public static double calcularB(List<Double> xs, List<Double> ys) {
+        int n = xs.size();
+
+        double sumX = 0;
+        double sumY = 0;
+        double sumXY = 0;
+        double sumX2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            double x = xs.get(i);
+            double y = ys.get(i);
+
+            sumX += x;
+            sumY += y;
+            sumXY += x * y;
+            sumX2 += x * x;
+        }
+
+        double numerador = n * sumXY - (sumX * sumY);
+        double denominador = n * sumX2 - (sumX * sumX);
+
+        return numerador / denominador;
+    }
+}
